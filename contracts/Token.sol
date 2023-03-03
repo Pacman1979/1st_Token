3e9185e316d07eeab46f2e5664b8788df05655d5
@Pacman1979 <cryptotraderphil@protonmail.com>
2023-03-03 16:36:39 +1000

    w2fr - Test Contract finished

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,8 +1,17 @@
 // SPDX-License-Identifier: UNLICENSE
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.17;
 
 import "hardhat/console.sol";
 
 contract Token {
-	string public name = "My Token";
+	string public name;
+	string public symbol;
+	uint256 public decimals = 18;
+	uint256 public totalSupply;
+
+	constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
+		name = _name;
+		symbol = _symbol;
+		totalSupply = _totalSupply * (10**decimals);
+	}
 }
--- test/Token.js
+++ test/Token.js
@@ -2,17 +2,52 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 // the above line pulls the ethers library from the hardhat library...
+const tokens = (n) => {
+	return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
 describe('Token', () => {
-	// Tests go inside here...
-	it('has a name', async () => {
-		// Fetch Token from Blockchain with Ethers.js (2 step)...
+	let token
+
+	beforeEach(async () => {
+		// Code goes here... for running before each 'it' function...
+		// instead of duplicating the following lines within the 'it' function..
+		// Fetch Token from Blockchain with Ethers.js (2 step process)...
 		// This imports/gets the contract itself but...
 		const Token = await ethers.getContractFactory('Token')
-		// To get a "deployed instance of that contract" we need this...
-		let token = await Token.deploy()
-		// Read token name...
-		const name = await token.name()
-		// Check that name is correct...
-		expect(name).to.equal('My Token')
+		// ...to get a "deployed instance of that contract" we need this...
+		token = await Token.deploy('Dapp University', 'DAPP', '1000000')
+	})
+
+	describe('Deployment', () => {
+		const name = 'Dapp University'
+		const symbol = 'DAPP'
+		const decimals = '18'
+		const totalSupply = tokens('1000000')
+
+	// Describe Spending...
+
+	// Describe approving...
+
+	// Describe...
+	
+	// Tests go inside here...
+		it('has correct name', async () => {
+		// 2 lines combined into 1 - Read token name and...
+		// ...check that name is correct...
+			expect(await token.name()).to.equal(name)
+		})
+
+		it('has correct symbol', async () => {
+			expect(await token.symbol()).to.equal(symbol)
+		})
+
+		it('has correct decimals', async () => {
+			expect(await token.decimals()).to.equal(decimals)
+		})
+
+		it('has correct total supply', async () => {
+			expect(await token.totalSupply()).to.equal(totalSupply)
+		})
 	})
 })
